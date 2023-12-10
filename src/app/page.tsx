338566d09e3f40d0d367d8d08c16737fd5ccb459
Det <nguyenthanhdatvn777@gmail.com>
2023-12-10 13:49:37 +0700

    fix typo in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,7 @@
-'use client'
-import Link from 'next/link'
+"use client"
+import Link from "next/link"
 //import { usePathname } from "next/navigation";
-import NavBar from './components/NavBar';
-
-
-
-// export function isHomePage(){
-//   const pathName = usePathname();
-//   const showHeader = pathName === '/' ? false : true;
-// }
+import NavBar from "./components/NavBar";
 
 export default function Home() {
   return (
