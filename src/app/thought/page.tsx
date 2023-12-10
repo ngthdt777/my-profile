632fc51b96e8d305089bb1eaf15dfd3eebed0d9d
Det <nguyenthanhdatvn777@gmail.com>
2023-12-10 19:48:35 +0700

    Temp remove ThoughtList

--- src/app/thought/page.tsx
+++ src/app/thought/page.tsx
@@ -11,7 +11,7 @@ export default function Thought() {
                     </small>
                 </p>
             </div>
-            <ThoughtList/>
+            {/* <ThoughtList/> */}
         </main>
     )
 }
