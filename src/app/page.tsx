59cd193931e6d5f29b44ad90d5ec9dab48b62a2c
Det <nguyenthanhdatvn777@gmail.com>
2023-12-10 13:27:33 +0700

    Comment out usePathname

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,14 @@
 'use client'
 import Link from 'next/link'
-import { usePathname } from "next/navigation";
+//import { usePathname } from "next/navigation";
 import NavBar from './components/NavBar';
 
 
 
-export function isHomePage(){
-  const pathName = usePathname();
-  const showHeader = pathName === '/' ? false : true;
-}
+// export function isHomePage(){
+//   const pathName = usePathname();
+//   const showHeader = pathName === '/' ? false : true;
+// }
 
 export default function Home() {
   return (
